50428f3fef051e491d86ea10679d22c21624dc4e
ScriptedSnark <ScriptedSnark@users.noreply.github.com>
2020-12-12 22:05:37 +0500

    Optimization for BSSetup form

--- Forms/BSSetup.cs
+++ Forms/BSSetup.cs
@@ -115,14 +115,29 @@ MessageBoxDefaultButton.Button1);
 
         async private void Button2_Click(object sender, EventArgs e)
         {
-            FadeOut(this, 2);
-            GameSelection Form2 = new GameSelection();
-            Form2.Opacity = 0.0;
-            Form2.Location = this.Location;
-            Form2.Show();
-            FadeIn(Form2, 2);
-            await Task.Delay(50);
-            Close();
+            GameSelection GS = (GameSelection)Application.OpenForms["GameSelection"];
+            if (GS == null) // optimizator activated, если форма не была создана, то давай уже создавайся
+            {
+                FadeOut(this, 2);
+                GameSelection GameSelection = new GameSelection(); // Создание нового экземпляра формы
+                GameSelection.Show(); // Отображаю форму
+                GameSelection.Opacity = 0.0;
+                GameSelection.Location = this.Location;
+                await Task.Delay(50);
+                FadeIn(GameSelection, 2);
+                await Task.Delay(50);
+                Hide();
+            }
+            else
+            {
+                GS.Activate(); // АГА ПОПАВСЯ, ТЫ ДУМАЛ МНЕ ТУТ ОПЕРАТИВУ НЕМНОГО ЗАНЯТЬ?
+                GS.Opacity = 0.0;
+                GS.Location = this.Location;
+                await Task.Delay(50);
+                FadeIn(GS, 2);
+                await Task.Delay(50);
+                Hide();
+            }
         }
 
         private void Button2_MouseEnter(object sender, EventArgs e)
